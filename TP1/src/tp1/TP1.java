fd2ddaad2ff7dd710c1f6a1648ac65eafc4a6817
Maximiliano David Ducoli <maxiducoli@gmail.com>
2025-09-10 21:53:35 -0300

    TP 1
    Programación II TUPD UTN

---
+++ TP1/src/tp1/TP1.java
@@ -0,0 +1,81 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+
+package tp1;
+
+import java.util.Scanner;
+
+/**
+ *
+ * @author mdd_1
+ */
+
+public class TP1 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        //System.out.println("Hola mundo");
+        
+        //String nombre = "Maximiliano";
+        //int edad = 48;
+        //double altuda = 1.82;
+        //boolean estudiante = true;
+        //System.out.println(nombre);
+        //System.out.println(edad);
+        //System.out.println(altuda);
+        //System.out.println(estudiante);
+        //Scanner input = new Scanner(System.in); 
+        //String nombre;
+        //System.out.println("Por favor, ingrese su nombre: ");
+        //nombre = input.nextLine();
+        //System.out.println(nombre);
+        //System.out.println("Por favor, ingrese su edad: ");
+        //int edad;
+        //edad = input.nextInt();
+        //System.out.println("Su nombre es: " + nombre + " y su edad es: " + edad);
+        
+        //Scanner input = new Scanner(System.in); 
+        //int numero1;
+        //System.out.println("Por favor, ingrese un número: ");
+        //numero1 = input.nextInt();
+        //System.out.println("Por favor, ingrese otro número: ");
+        //int numero2;
+        //numero2 = input.nextInt();
+                
+        //int suma = numero1 + numero2;
+        //int resta = numero1 - numero2;
+        //double multiplicacion = numero1 * numero2;
+        //double division = numero1 / numero2;
+        //System.out.println("La suma es: " + suma);
+        //System.out.println("La resta es: " + resta);
+        //System.out.println("La multiplicacion es: " + multiplicacion);
+        //System.out.println("La división es: " + division);
+        
+        //System.out.println("Nombre: Juan Pérez\nEdad: 30 años\nDirección:\"Calle Falsa 123\"");
+     
+        Scanner input = new Scanner(System.in); 
+        int numero1;
+        System.out.println("Por favor, ingrese un número: ");
+        numero1 = input.nextInt();
+        int numero2;
+        System.out.println("Por favor, ingrese otro número: ");
+        numero2 = input.nextInt();
+        
+        int division1 = numero1 / numero2;
+        double division2 = numero1 / numero2;
+        
+        System.out.println("La división 1 da: " + division1);
+        System.out.println("La división 2 da: " + division2);
+        
+        
+    }
+    
+}
+
+
+
